d6a44b636bc06525dfc687cc9488088347095382
sergiovonsik <nicolasvonsik@gmail.com>
2025-02-17 18:42:44 -0300

    ...

--- backend/api/views.py
+++ backend/api/views.py
@@ -222,7 +222,7 @@ class MercadoPagoTicket(ModelViewSet):
             "items": [
                 {
                     "title": f"Pass for: {ticket_data.get('type_of_service')}",
-                    "quantity": int(request.data.get("price")),
+                    "quantity": 1,
                     "unit_price": int(request.data.get("price")),
                 }
             ],
