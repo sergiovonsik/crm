1c0116ca3990592c441cd7bcc67fbdc89e758e4d
sergiovonsik <nicolasvonsik@gmail.com>
2025-03-02 12:07:36 -0300

    cxzcxz

--- worker/worker.py
+++ worker/worker.py
@@ -6,18 +6,18 @@ URL = "https://crm-udrl.onrender.com/woker_greeting/"
 
 
 def send_request():
+
     try:
         response = requests.post(URL, json={"greet": "hello"})
-        print(f"Response ({response.status_code}): {response.text}")
+        print(f"Response {time.strftime('%H:%M:%S', time.localtime())} ({response.status_code}): {response.text}")
     except requests.RequestException as e:
         print(f"Request failed: {e}")
-        print(time.now())
+
 
 
 # Schedule the request every 10 minutes
 schedule.every(11).minutes.do(send_request)
 
-while True:
-    print("worker initialized")
+while True: 
     schedule.run_pending()
-    time.sleep(60*11)
+    time.sleep(60*6)
