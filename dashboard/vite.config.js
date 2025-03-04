c8a242ac981270f1ffe5c798d1bc22e1cc225c52
sergiovonsik <nicolasvonsik@gmail.com>
2025-03-04 12:57:20 -0300

    fix url bug

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
-    historyApiFallback: true,
-    port: 8005,
+    historyApiFallback: true, // Ensures React Router works in development
   }
-})
+});
