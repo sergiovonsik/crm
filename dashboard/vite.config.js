df6098e1075f5426c1acfe6916d2af87dc0fa60e
sergiovonsik <nicolasvonsik@gmail.com>
2025-03-04 13:05:16 -0300

    bug.2

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Ensures correct path resolution
+  build: {
+    outDir: 'build', // Make sure it's named 'build' for Render
+  },
   server: {
-    historyApiFallback: true, // Ensures React Router works in development
-  }
+    historyApiFallback: true,
+  },
 });
