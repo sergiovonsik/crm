22077ba37c9da28561bec979148a27c6dd39d289
sergiovonsik <nicolasvonsik@gmail.com>
2025-03-04 13:07:19 -0300

    ....

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/', // Ensures correct path resolution
-  build: {
-    outDir: 'build', // Make sure it's named 'build' for Render
-  },
   server: {
     historyApiFallback: true,
   },
